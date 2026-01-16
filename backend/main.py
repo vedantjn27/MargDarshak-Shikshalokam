d02face63b29c7be23b0d272708556fab9656bc8
vedantjn27 <vedantjain273@gmail.com>
2026-01-16 09:43:44 +0530

    cors

--- backend/main.py
+++ backend/main.py
@@ -76,7 +76,7 @@ app = FastAPI(
 # -------------------- CORS --------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
